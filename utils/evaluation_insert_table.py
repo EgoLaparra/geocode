c354fcd9475947aa0843c3d41c707e747bfb1542
JerryZeyu <zzy19951013@gmail.com>
2020-12-08 09:36:29 -0700

    revise the codes

--- utils/evaluation_insert_table.py
+++ utils/evaluation_insert_table.py
@@ -25,10 +25,17 @@ with open('eval_preds_score.json', 'r') as file:
 
 entity2desc = pickle_load_large_file('../../geocode-data/collection_samples/model_input_desc_dev.pkl')
 entityIds = list(entity2desc.keys())
-for key, value in output_raw.items():
-    for idx, prediction in enumerate(value):
-        entity_id = entityIds[idx]
-        geometry = geom.from_text("LINESTRING(%s)" % ", ".join([" ".join(map(str, point)) for e1, row in enumerate(prediction) for e2, point in enumerate(row) if not (e1 == 1 and e2 == 1)] + ["%s %s" % (prediction[0][0][0],prediction[0][0][1])]))
+value = output_raw['preds_score_']
+for idx, prediction in enumerate(value):
+    entity_id = entityIds[idx]
+    geometry = geom.from_text("LINESTRING(%s)" % ", ".join([" ".join(map(str, point)) for e1, row in enumerate(prediction) for e2, point in enumerate(row) if not (e1 == 1 and e2 == 1)] + ["%s %s" % (prediction[0][0][0],prediction[0][0][1])]))
 
 
-        geom.database.insert_in_table("output_table", idx, entity_id, geometry)
+    geom.database.insert_in_table("output_table", idx, entity_id, geometry)
+# for key, value in output_raw.items():
+#     for idx, prediction in enumerate(value):
+#         entity_id = entityIds[idx]
+#         geometry = geom.from_text("LINESTRING(%s)" % ", ".join([" ".join(map(str, point)) for e1, row in enumerate(prediction) for e2, point in enumerate(row) if not (e1 == 1 and e2 == 1)] + ["%s %s" % (prediction[0][0][0],prediction[0][0][1])]))
+#
+#
+#         geom.database.insert_in_table("output_table", idx, entity_id, geometry)
