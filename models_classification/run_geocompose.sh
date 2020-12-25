75fadd167bbbb0aa3ddaee5ad33ad70f63cef841
JerryZeyu <zzy19951013@gmail.com>
2020-12-25 13:36:51 -0700

    revise the codes

--- models_classification/run_geocompose.sh
+++ models_classification/run_geocompose.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=224gb:np100s=1:os7=True
 ### Specify a name for the job
-#PBS -N cla60_50
+#PBS -N cla50_50
 ### Specify the group name
 #PBS -W group_list=nlp
 ### Used if job requires partial node only
@@ -13,8 +13,8 @@
 #PBS -l cput=140:00:00
 ### Walltime is how long your job will run
 #PBS -l walltime=5:00:00
-#PBS -e /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification/logs/error_60_epoch50
-#PBS -o /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification/logs/output_60_epoch50
+#PBS -e /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification/logs/error_50_epoch50
+#PBS -o /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification/logs/output_50_epoch50
 
 #####module load cuda80/neuralnet/6/6.0
 #####module load cuda80/toolkit/8.0.61
@@ -30,11 +30,11 @@ singularity exec --nv /xdisk/bethard/mig2020/extra/zeyuzhang/image/hpc-ml_centos
 --do_eval \
 --eval_all_checkpoints \
 --data_dir /home/u12/zeyuzhang/Geo_Compositional/geocode-data/collection_samples \
---n_labels 19 \
+--n_labels 2501 \
 --learning_rate 5e-6 \
 --num_train_epochs 50 \
 --max_seq_length 400 \
 --per_gpu_train_batch_size 8 \
 --gradient_accumulation_steps 8 \
 --save_steps 1000 \
---output_dir /xdisk/bethard/mig2020/xdisk/zeyuzhang/Geo-Compositional_classification_relative/output_26_epoch50/
+--output_dir /xdisk/bethard/mig2020/xdisk/zeyuzhang/Geo-Compositional_classification_relative/output_50_epoch50/
