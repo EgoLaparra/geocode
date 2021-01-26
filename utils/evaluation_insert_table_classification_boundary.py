1833b9151db6b3ec0153490156334a3c9766ae1b
JerryZeyu <zzy19951013@gmail.com>
2021-01-26 11:47:39 -0700

    revise the codes

--- utils/evaluation_insert_table_classification_boundary.py
+++ utils/evaluation_insert_table_classification_boundary.py
@@ -41,14 +41,14 @@ def pickle_load_large_file(filepath):
 
 geom = Geometries()
 
-with open('classification_relative_boundary_results/eval_preds_26_epoch300.json', 'r') as file:
+with open('classification_relative_boundary_results/eval_preds_26_epoch400.json', 'r') as file:
     output_raw = json.load(file)
 
 entity2desc = pickle_load_large_file('../../geocode-data/collection_samples/model_input_desc_classification_relative_boundary_26_dev.pkl')
 entityID2boundary = pickle_load_large_file('../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_26_dev.pkl')
 entityIds = list(entity2desc.keys())
 print(output_raw.keys())
-value = output_raw['preds_Compositional_classification_relative_boundary/output_26_epoch300']
+value = output_raw['preds_Compositional_classification_relative_boundary/output_26_epoch400']
 #print(value)
 print(len(value))
 
