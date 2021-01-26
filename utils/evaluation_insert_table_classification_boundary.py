ea71bfb72e0e82bb20f602bc0ad0b9db7c4b9a5b
JerryZeyu <zzy19951013@gmail.com>
2021-01-26 11:56:34 -0700

    revise the codes

--- utils/evaluation_insert_table_classification_boundary.py
+++ utils/evaluation_insert_table_classification_boundary.py
@@ -41,14 +41,14 @@ def pickle_load_large_file(filepath):
 
 geom = Geometries()
 
-with open('classification_relative_boundary_results/eval_preds_50_epoch50.json', 'r') as file:
+with open('classification_relative_boundary_results/eval_preds_100_epoch50.json', 'r') as file:
     output_raw = json.load(file)
 
-entity2desc = pickle_load_large_file('../../geocode-data/collection_samples/model_input_desc_classification_relative_boundary_50_dev.pkl')
-entityID2boundary = pickle_load_large_file('../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_50_dev.pkl')
+entity2desc = pickle_load_large_file('../../geocode-data/collection_samples/model_input_desc_classification_relative_boundary_100_dev.pkl')
+entityID2boundary = pickle_load_large_file('../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_100_dev.pkl')
 entityIds = list(entity2desc.keys())
 print(output_raw.keys())
-value = output_raw['preds_Compositional_classification_relative_boundary/output_50_epoch50']
+value = output_raw['preds_Compositional_classification_relative_boundary/output_100_epoch50']
 #print(value)
 print(len(value))
 
@@ -56,7 +56,7 @@ print(len(value))
 for idx, prediction in enumerate(value):
     entity_id = entityIds[idx]
     min_bound, max_bound = entityID2boundary[entity_id]
-    prediction_values = index_to_tile_relative(prediction, 50, min_bound, max_bound)
+    prediction_values = index_to_tile_relative(prediction, 100, min_bound, max_bound)
     geometry = make_polygon(geom, prediction_values)
     #print(prediction_values)
     #prediction_values = [[[prediction_values[0] - 26 / 2, prediction_values[1] - 26 / 2], [prediction_values[0] - 26 / 2, prediction_values[1] + 26 / 2]], [[prediction_values[0] + 26 / 2, prediction_values[1] + 26 / 2], [prediction_values[0] + 26 / 2, prediction_values[1] - 26 / 2]]]
