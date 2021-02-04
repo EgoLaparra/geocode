21dd47059196281766c22d630af4452b95a2a99e
JerryZeyu <zzy19951013@gmail.com>
2021-02-04 12:55:17 -0700

    revise the codes

--- utils/evaluation_insert_table_classification_boundary.py
+++ utils/evaluation_insert_table_classification_boundary.py
@@ -59,6 +59,7 @@ for idx, prediction in enumerate(value):
     prediction_values = index_to_tile_relative(prediction, 10, min_bound, max_bound)
     geometry = make_polygon(geom, prediction_values)
     if entity_id == "GL155_072":
+        print(prediction_values)
         print(geometry)
     #print(prediction_values)
     #prediction_values = [[[prediction_values[0] - 26 / 2, prediction_values[1] - 26 / 2], [prediction_values[0] - 26 / 2, prediction_values[1] + 26 / 2]], [[prediction_values[0] + 26 / 2, prediction_values[1] + 26 / 2], [prediction_values[0] + 26 / 2, prediction_values[1] - 26 / 2]]]
