98dfbfaabff29e6bf4fe281d4bdaae87057b54dd
JerryZeyu <zzy19951013@gmail.com>
2021-02-15 20:29:45 -0700

    revise the codes

--- utils/get_simplify_shapes_classification_relative_boundary_partialText.py
+++ utils/get_simplify_shapes_classification_relative_boundary_partialText.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
             entity_central_point = geom.get_centrality(entity_geometry, metric="centroid")
             entity_central_coordinates = geom.get_coordinates(entity_central_point)
             #geometries.append(entity_geometry)
-            limit_to_inner_boundaries(geom, geometries)
+            #limit_to_inner_boundaries(geom, geometries)
             min_bound, max_bound = geometry_group_bounds(geom, geometries, squared=True)
             min_bound = (max(min_bound[0], -179.999999), max(min_bound[1], -89.999999))
             max_bound = (min(max_bound[0], 179.999999), min(max_bound[1], 89.999999))
