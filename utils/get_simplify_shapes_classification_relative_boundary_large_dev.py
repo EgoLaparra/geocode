1aa713f943449bcba3439683dfca1a6ca2065a4d
JerryZeyu <zzy19951013@gmail.com>
2021-02-16 20:19:51 -0700

    revise the codes

--- utils/get_simplify_shapes_classification_relative_boundary_large_dev.py
+++ utils/get_simplify_shapes_classification_relative_boundary_large_dev.py
@@ -1,9 +1,9 @@
 from geometries import Geometries
 from preprocess import coord_to_index_relative, geometry_group_bounds, limit_to_inner_boundaries
-from collections import OrderedDict
 from lxml import etree
 from tqdm import tqdm
 from itertools import chain
+from collections import OrderedDict
 import argparse
 import pickle
 import sys
@@ -42,32 +42,31 @@ def get_text(node):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--xml_filepath_train', default='../../geocode-data/collection_samples/train_samples.xml', type=str,
+    parser.add_argument('--xml_filepath_dev', default='../../geocode-data/collection_samples/dev_samples.xml', type=str,
                         help='path of data collections')
     parser.add_argument('--sample_size', default=50, type=int,
                         help='number of sample datas')
-    parser.add_argument('--output_target_train', default='../../geocode-data/collection_samples/model_input_target_classification_relative_boundary_10_train.pkl', type=str,
+    parser.add_argument('--output_target_dev', default='../../geocode-data/collection_samples/model_input_target_classification_relative_boundary_10_dev.pkl', type=str,
                         help='path of data collections samples')
-    parser.add_argument('--output_paras_train', default='../../geocode-data/collection_samples/model_input_paras_classification_relative_boundary_10_train.pkl',
+    parser.add_argument('--output_paras_dev', default='../../geocode-data/collection_samples/model_input_paras_classification_relative_boundary_10_dev.pkl',
                         type=str,
                         help='path of data collections samples')
-    parser.add_argument('--output_desc_train',
-                        default='../../geocode-data/collection_samples/model_input_desc_classification_relative_boundary_10_train.pkl',
+    parser.add_argument('--output_desc_dev',
+                        default='../../geocode-data/collection_samples/model_input_desc_classification_relative_boundary_10_dev.pkl',
                         type=str,
                         help='path of data collections samples')
-    parser.add_argument('--output_boundary_train',
-                        default='../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_10_train.pkl',
+    parser.add_argument('--output_boundary_dev',
+                        default='../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_10_dev.pkl',
                         type=str,
                         help='path of data collections samples')
     parser.add_argument('--polygon_size',
                         default=10,
                         type=int,
                         help="polygon size of coor_2_index")
-
     args = parser.parse_args()
     geom = Geometries()
 
-    entities = get_entities_fromXML(args.xml_filepath_train)
+    entities = get_entities_fromXML(args.xml_filepath_dev)
     print("entity numbers: ", len(entities))
     entityID2target = {}
     entityID2paras = {}
@@ -78,7 +77,7 @@ if __name__ == '__main__':
         print(entity_id)
         geometries = []
         try:
-            # process paras entities
+            ##process paras entities
             for p in entity.xpath('./p'):
                 for e, link in enumerate(p.xpath('./link')):
                     link_geometry = geom.get_entity_geometry(link)
@@ -97,7 +96,8 @@ if __name__ == '__main__':
                     link_geometry = geom.get_entity_geometry(link)
                     link_central_point = geom.get_centrality(link_geometry, metric="centroid")
                     link_central_x, link_central_y = geom.get_coordinates(link_central_point)
-                    link_classification_label = coord_to_index_relative((link_central_x, link_central_y), args.polygon_size, min_bound, max_bound)
+                    link_classification_label = coord_to_index_relative((link_central_x, link_central_y),
+                                                                        args.polygon_size, min_bound, max_bound)
                     linkID2coordinates[linkID] = link_classification_label
                 pID2links[pID] = linkID2coordinates
 
@@ -106,7 +106,8 @@ if __name__ == '__main__':
             entity_central_point = geom.get_centrality(entity_geometry, metric="centroid")
             entity_central_x, entity_central_y = geom.get_coordinates(entity_central_point)
             print('entity central point: ', (entity_central_x, entity_central_y))
-            entity_classification_label = coord_to_index_relative((entity_central_x, entity_central_y), args.polygon_size, min_bound, max_bound)
+            entity_classification_label = coord_to_index_relative((entity_central_x, entity_central_y),
+                                                                  args.polygon_size, min_bound, max_bound)
             print('classification_label: ', entity_classification_label)
 
             ##process entity description
@@ -117,7 +118,7 @@ if __name__ == '__main__':
             entityID2desc[entity_id] = text
             entityID2paras[entity_id] = pID2links
             entityID2target[entity_id] = entity_classification_label
-            entityID2boundary[entity_id] = [min_bound, max_bound]
+            entityID2boundary[entity_id] = [min_bound,max_bound]
         except Exception as e:
             print("Error processing %s" % (entity_id))
             print(e)
@@ -126,8 +127,8 @@ if __name__ == '__main__':
     print(len(list(entityID2target.keys())))
     print(len(list(entityID2paras.keys())))
     assert len(list(entityID2desc.keys())) == len(list(entityID2target.keys())) == len(list(entityID2paras.keys()))
-    pickle_dump_large_file(entityID2target, args.output_target_train)
-    pickle_dump_large_file(entityID2paras, args.output_paras_train)
-    pickle_dump_large_file(entityID2desc, args.output_desc_train)
-    pickle_dump_large_file(entityID2boundary, args.output_boundary_train)
     geom.close_connection()
+    pickle_dump_large_file(entityID2target, args.output_target_dev)
+    pickle_dump_large_file(entityID2paras, args.output_paras_dev)
+    pickle_dump_large_file(entityID2desc, args.output_desc_dev)
+    pickle_dump_large_file(entityID2boundary, args.output_boundary_dev)
