87656c176878772636d8d53ee109a9cb73cecfda
JerryZeyu <zzy19951013@gmail.com>
2021-02-19 12:52:45 -0700

    revise the codes

--- utils/evaluation_insert_table_classification_boundary.py
+++ utils/evaluation_insert_table_classification_boundary.py
@@ -48,7 +48,8 @@ entity2desc = pickle_load_large_file('../../geocode-data/collection_samples/mode
 entityID2boundary = pickle_load_large_file('../../geocode-data/collection_samples/model_input_boundary_classification_relative_boundary_10_dev.pkl')
 entityIds = list(entity2desc.keys())
 print(output_raw.keys())
-value = output_raw['preds_Compositional_classification_relative_boundary/output_10_large']
+#value = output_raw['preds_Compositional_classification_relative_boundary/output_10_large']
+value = output_raw['preds_11000']
 #print(value)
 print(len(value))
 
