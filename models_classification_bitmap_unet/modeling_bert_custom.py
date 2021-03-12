2bc3a7cc942e47d7f75cc629d8390bb5481d4120
JerryZeyu <zzy19951013@gmail.com>
2021-03-12 11:50:52 -0700

    revise the codes

--- models_classification_bitmap_unet/modeling_bert_custom.py
+++ models_classification_bitmap_unet/modeling_bert_custom.py
@@ -1026,9 +1026,10 @@ class BertForSequenceClassification(BertPreTrainedModel):
         x = self.up4(x, x1)
         temp_logits = self.outc(x)
         print("temp_logits: ", temp_logits.shape)
-        temp_logits = temp_logits.view(batch_size, pairs_num, -1)
-        print("temp_logits: ", temp_logits.shape)
-        logits = torch.max(temp_logits, dim=1).values
+        logits=temp_logits
+        #temp_logits = temp_logits.view(batch_size, pairs_num, -1)
+        #print("temp_logits: ", temp_logits.shape)
+        #logits = torch.max(temp_logits, dim=1).values
         outputs = (logits,) + outputs[2:]
         #print('logits shape: ',logits.shape)
         if labels is not None:
