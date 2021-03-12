a2264717465b589e90d7485f750781f602d410fc
JerryZeyu <zzy19951013@gmail.com>
2021-03-12 11:49:59 -0700

    revise the codes

--- models_classification_bitmap_unet/modeling_bert_custom.py
+++ models_classification_bitmap_unet/modeling_bert_custom.py
@@ -1025,6 +1025,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         temp_logits = self.outc(x)
+        print("temp_logits: ", temp_logits.shape)
         temp_logits = temp_logits.view(batch_size, pairs_num, -1)
         print("temp_logits: ", temp_logits.shape)
         logits = torch.max(temp_logits, dim=1).values
