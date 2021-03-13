5a9941622292f3cf68d857ec0866d6eabdfe664a
JerryZeyu <zzy19951013@gmail.com>
2021-03-12 19:04:39 -0700

    revise the codes

---
+++ models_classification_bitmap_unet_deeper/run_geocompose.sh
@@ -0,0 +1,20 @@
+python run_glue.py \
+--task_name geocompose \
+--model_type bert \
+--model_name_or_path bert-base-uncased \
+--do_train \
+--do_eval \
+--do_relative \
+--do_boundary \
+--num_tiles 26 \
+--num_links_topairs 6 \
+--eval_all_checkpoints \
+--data_dir ../geocode-data/collection_samples \
+--n_labels 676 \
+--learning_rate 5e-6 \
+--num_train_epochs 100 \
+--max_seq_length 400 \
+--per_gpu_train_batch_size 8 \
+--gradient_accumulation_steps 8 \
+--save_steps 1000 \
+--output_dir output_bitmap_26_6_epoch100/
