0d1d277b667810f1efe335c1a720927cf580dc55
JerryZeyu <zzy19951013@gmail.com>
2021-03-16 20:06:53 -0700

    revise the codes

--- utils/evaluation_insert_table_classification_bitmap_functionAnalysis.py
+++ utils/evaluation_insert_table_classification_bitmap_functionAnalysis.py
@@ -27,14 +27,27 @@ entity2target = pickle_load_large_file('/xdisk/bethard/zeyuzhang/Geo-Composition
 entityID2boundary = pickle_load_large_file('/xdisk/bethard/zeyuzhang/Geo-Compositional_data/model_input_boundary_classification_bitmap_boundary_70_dev.pkl')
 
 
-
+num_table_inserted = 0
 for idx,(entity_id, target_bitmap) in enumerate(entity2target.items()):
     min_bound, max_bound = entityID2boundary[entity_id]
     min_bound = (max(min_bound[0], -179.999999), max(min_bound[1], -89.999999))
     max_bound = (min(max_bound[0], 179.999999), min(max_bound[1], 89.999999))
     grid = bounded_grid(geom, 70, min_bound, max_bound)
     print("entity_id: ", entity_id)
-    #print("target_bitmap: ", target_bitmap)
+
+    temp_flag = 0
+    over_flag = []
+    for each_row in target_bitmap:
+        for item in each_row:
+            if item > target_bitmap:
+                temp_flag = 1
+    if temp_flag == 0:
+        continue
+    # if 0 not in over_flag:
+    #    continue
+    num_table_inserted += 1
+    print(entity_id)
+
     target_geometry = bitmap_to_geometry(geom, grid, target_bitmap)
     #print(prediction_values)
     #prediction_values = [[[prediction_values[0] - 26 / 2, prediction_values[1] - 26 / 2], [prediction_values[0] - 26 / 2, prediction_values[1] + 26 / 2]], [[prediction_values[0] + 26 / 2, prediction_values[1] + 26 / 2], [prediction_values[0] + 26 / 2, prediction_values[1] - 26 / 2]]]
@@ -46,7 +59,7 @@ for idx,(entity_id, target_bitmap) in enumerate(entity2target.items()):
     #geometry = geom.from_text("POLYGON((%s))" % ", ".join([" ".join(map(str, point)) for e1, row in enumerate(prediction_values) for e2, point in enumerate(row)] + ["%s %s" % (prediction_values[0][0][0],prediction_values[0][0][1])]))
 
     geom.database.insert_in_table("output_table", idx, entity_id, target_geometry)
-
+print(num_table_inserted)
 
 # for key, value in output_raw.items():
 #     for idx, prediction in enumerate(value):
