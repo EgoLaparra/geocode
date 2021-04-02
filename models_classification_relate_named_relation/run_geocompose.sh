7457dc310a510d9c957e4aef837301013d43a6b2
JerryZeyu <zzy19951013@gmail.com>
2021-04-01 21:03:28 -0700

    revise codes

--- models_classification_relate_named_relation/run_geocompose.sh
+++ models_classification_relate_named_relation/run_geocompose.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=224gb:np100s=1:os7=True
 ### Specify a name for the job
-#PBS -N rela_epo5
+#PBS -N rela_epo10
 ### Specify the group name
 #PBS -W group_list=nlp
 ### Used if job requires partial node only
@@ -13,8 +13,8 @@
 #PBS -l cput=840:00:00
 ### Walltime is how long your job will run
 #PBS -l walltime=30:00:00
-#PBS -e /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification_relate_named_relation/logs/error_named_relation_epoch5
-#PBS -o /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification_relate_named_relation/logs/output_named_relation_epoch5
+#PBS -e /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification_relate_named_relation/logs/error_named_relation_epoch10
+#PBS -o /home/u12/zeyuzhang/Geo_Compositional/geocode/models_classification_relate_named_relation/logs/output_named_relation_epoch10
 
 #####module load cuda80/neuralnet/6/6.0
 #####module load cuda80/toolkit/8.0.61
@@ -32,9 +32,9 @@ singularity exec --nv /xdisk/bethard/zeyuzhang/hpc-ml_centos7-python3.7-transfor
 --data_dir /xdisk/bethard/zeyuzhang/Geo-Compositional_data/ \
 --n_labels 8 \
 --learning_rate 5e-6 \
---num_train_epochs 5 \
+--num_train_epochs 10 \
 --max_seq_length 400 \
 --per_gpu_train_batch_size 8 \
 --gradient_accumulation_steps 8 \
 --save_steps 200 \
---output_dir /xdisk/bethard/zeyuzhang/models_classification_relate_named_relation/output_epoch5/
+--output_dir /xdisk/bethard/zeyuzhang/models_classification_relate_named_relation/output_epoch10/
