4e0fdb72838baf54605013eee4085952a26f4c14
Steven Bethard <steven.bethard@gmail.com>
2024-10-22 08:42:09 -0500

    Factors out common diameter + start distance code

--- utils/operators.py
+++ utils/operators.py
@@ -3,10 +3,21 @@ import shapely
 import shapely.affinity
 
 
-def diameter(geometry: shapely.geometry.base.BaseGeometry) -> float:
+def diameter(geometry: shapely.geometry.base.BaseGeometry):
     coords = geometry.minimum_rotated_rectangle.exterior.coords
     corner0, corner1, corner2, _, _ = map(shapely.geometry.Point, coords)
     return max(corner0.distance(corner1), corner1.distance(corner2))
+
+
+def _diameter_and_start(geometry: shapely.geometry.base.BaseGeometry,
+                        distance: float = None) -> (float, float):
+    d = diameter(geometry)
+    radius = d / 2
+    if distance is None:
+        start_distance = radius
+    else:
+        start_distance = max(0.0, distance - radius)
+    return d, start_distance
 
 
 def chord_perpendicular_to_point(polygon: shapely.Polygon,
@@ -30,29 +41,29 @@ class GeoCardinal:
     azimuth: int
 
     def of(self,
-           polygon: shapely.Polygon,
-           distance: float = 0) -> shapely.Polygon:
-        length = distance + diameter(polygon)
-        y = polygon.centroid.y + length
+           geometry: shapely.geometry.base.BaseGeometry,
+           distance: float = None) -> shapely.Polygon:
+        d, start_distance = _diameter_and_start(geometry, distance)
         # line at azimuth 0 (i.e., North)
-        line = shapely.LineString([polygon.centroid, (polygon.centroid.x, y)])
+        y = geometry.centroid.y + start_distance + d
+        line = shapely.LineString([geometry.centroid, (geometry.centroid.x, y)])
         # rotate line to specified azimuth (negative = clockwise)
-        line = shapely.affinity.rotate(line, -self.azimuth, origin=polygon.centroid)
+        line = shapely.affinity.rotate(line, -self.azimuth, origin=geometry.centroid)
         # collect points along the arc
-        coordinates = [polygon.centroid]
+        coordinates = [geometry.centroid]
         for angle in range(-45, +45 + 1, 15):
             rotated_line = shapely.affinity.rotate(
-                line, -angle, origin=polygon.centroid)
+                line, -angle, origin=geometry.centroid)
             _, coordinate = rotated_line.coords
             coordinates.append(coordinate)
         # add the centroid again and construct the polygon
-        coordinates.append(polygon.centroid)
+        coordinates.append(geometry.centroid)
         result = shapely.Polygon(coordinates)
-        # if a distance is provided, remove the region nearest to the polygon
-        if distance:
-            result -= polygon.centroid.buffer(distance)
+        # if a distance is provided, remove the region nearest to the input
+        if distance is not None and start_distance > 0:
+            result -= geometry.centroid.buffer(start_distance)
         # keep only the region that does not overlap with the input
-        return result - polygon
+        return result - geometry
 
 
 North = GeoCardinal(azimuth=0)
@@ -67,11 +78,12 @@ NorthWest = GeoCardinal(azimuth=315)
 
 class Near:
     @staticmethod
-    def to(geometry: shapely.geometry.base.BaseGeometry, distance: float = 0):
-        total_distance = distance + diameter(geometry)
-        result = geometry.centroid.buffer(total_distance)
-        if distance:
-            result -= geometry.centroid.buffer(distance)
+    def to(geometry: shapely.geometry.base.BaseGeometry,
+           distance: float = None):
+        d, start_distance = _diameter_and_start(geometry, distance)
+        result = geometry.centroid.buffer(start_distance + d)
+        if distance is not None and start_distance > 0:
+            result -= geometry.centroid.buffer(start_distance)
         return result - geometry
 
 
